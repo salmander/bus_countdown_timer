7c45d60e3af2535295f044e6cead562fc9217b90
Salman Ahmed <salman.great@gmail.com>
2015-08-10 21:04:52 +0100

    bug fix

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var request = http.get(end_point, function(resp) {
     var body = ""; // contains json object
     // On data event
     resp.on('data', function(d) {
-        //console.log("Data event: " + d);
         console.log('Data received from HTTP');
+        //console.log("Data event: " + d);
         body += d;
     });
 
@@ -51,7 +51,7 @@ var request = http.get(end_point, function(resp) {
         one_six_three.countdown_seconds = moment(endDate).diff(startDate, 'seconds');
 
         // Construct 40 bus time in date time format
-        var endDate = new Date(startDate.toDateString() + " " + one_six_three.aimed_departure_time);
+        var endDate = new Date(startDate.toDateString() + " " + fourty.aimed_departure_time);
 
         // Calculate time difference (for 40) in seconds and append to the object
         fourty.countdown_seconds = moment(endDate).diff(startDate, 'seconds');
